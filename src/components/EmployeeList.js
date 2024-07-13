6b8d15c47feffa4ba45118c41dbe45384bb3b221
Shoaib Ashfaq <shoaibashfaq111@gmail.com>
2024-07-13 09:07:03 -0400

    Adds pagination logic to the all_employees route

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,25 +1,28 @@
-// src/components/EmployeeList.js
 import React, { useState, useEffect } from 'react';
-import EmployeeDetails from './EmployeeDetails.js';
 import { Link } from 'react-router-dom';
 
 function EmployeeList() {
   const [employees, setEmployees] = useState([]);
-  const [selectedEmployee, setSelectedEmployee] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
-    fetchEmployees();
-  }, []);
+    fetchEmployees(currentPage);
+  }, [currentPage]);
 
-  const fetchEmployees = async () => {
+  const fetchEmployees = async (page) => {
     try {
-      const response = await fetch('http://localhost:3000/api/all_employees');
+      const response = await fetch(`http://localhost:3000/api/all_employees?page=${page}`);
       const data = await response.json();
-      // console.log(data)
-      setEmployees(data);
+      setEmployees(data.employees);
+      setTotalPages(data.totalPages);
     } catch (error) {
       console.error('Error fetching employees:', error);
     }
+  };
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
   };
 
   return (
@@ -35,6 +38,21 @@ function EmployeeList() {
             <p>{employee.jobRole}</p>
           </Link>
         ))}
+      </div>
+      <div className="pagination">
+        <button 
+          onClick={() => handlePageChange(currentPage - 1)} 
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <span>Page {currentPage} of {totalPages}</span>
+        <button 
+          onClick={() => handlePageChange(currentPage + 1)} 
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
